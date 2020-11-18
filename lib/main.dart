de1083772949805449cbf81c4b4a0176342040ad
Raphael Passos <raphael251@hotmail.com>
2020-11-18 15:08:58 -0300

    chore: creating a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'qual é a sua cor favorita?',
+      'qual é o seu animal favorito?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
